756d7cb2534f04a02baa83e593a5b1c365d0abe7
JulianPle <julian.plepla@gmx.de>
2023-05-25 17:27:11 +0200

    popup und klick auf Innsbruck simulieren

--- main.js
+++ main.js
@@ -37,6 +37,7 @@ async function showForecast(url, latlng) {
     console.log(jsondata, latlng);
     let current = jsondata.properties.timeseries[0].data.instant.details;
     console.log(current);
+    let timeseries = jsondata.properties.timeseries;
     let timestamp = new Date(jsondata.properties.meta.updated_at).toLocaleString();
     let markup = `
     <h4>Wetter für ${latlng.lat.toFixed(4)}, ${latlng.lng.toFixed(4)} ${timestamp}</h4>
@@ -48,15 +49,27 @@ async function showForecast(url, latlng) {
         <tr><td>Windrichtung (Grad)</td><td>${current.wind_from_direction}</td></tr>
         <tr><td>Windgeschwindigkeit (km/h)</td><td>${current.wind_speed}</td></tr>
     </table>
-    
     `;
+    // Wettersymbole
+    for (let i=0; i<=24; i+=3){
+        console.log(timeseries[i]);
+        let icon = timeseries[i].data.next_1_hours.summary.symbol_code;
+        let image = `icons/${icon}.svg`;
+        markup += `<img src="${image}" style="width:32px;" title="${timeseries[i].time.toLocaleString()}">`
+        console.log(icon);
+    }
+
     L.popup().setLatLng(latlng).setContent(markup).openOn(map);
 
 }
 // auf Kartenklick reagieren 
-map.on("click", function(evt){
+map.on("click", function (evt) {
     //console.log(evt.latlng.lat),
     let url = `https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${evt.latlng.lat}&lon=${evt.latlng.lng}`;
     showForecast(url, evt.latlng);
 
 });
+//klick auf Innsbruck simulieren
+map.fireEvent("click", {
+    latlng: L.latLng(ibk.lat, ibk.lng)
+})

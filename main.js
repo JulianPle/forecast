0c511debae879242fe68b768faa6036db05c7aa9
JulianPle <julian.plepla@gmx.de>
2023-05-25 15:56:10 +0200

    foreCast Funktion aufrufen und ergebnis anzeigen

--- main.js
+++ main.js
@@ -38,6 +38,8 @@ async function showForecast(url, latlng) {
 }
 // auf Kartenklick reagieren 
 map.on("click", function(evt){
-    console.log(evt.latlng.lat),
-    console.log(`https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${evt.latlng.lat}&lon=${evt.latlng.lng}`)
+    //console.log(evt.latlng.lat),
+    let url = `https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${evt.latlng.lat}&lon=${evt.latlng.lng}`;
+    showForecast(url, evt.latlng);
+
 });
